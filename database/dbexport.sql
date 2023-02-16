bc2d6885a9f6c739196f7c1a05620bee858838f6
xdesign-mikesmith <mike.smith@xdesign.com>
2023-02-16 10:42:36 +0000

    db export added

---
+++ database/dbexport.sql
@@ -0,0 +1,68 @@
+/*
+-- Query: select * from acronyms
+LIMIT 0, 1000
+
+-- Date: 2023-02-16 10:40
+*/
+INSERT INTO `` (`id`,`acronym`,`meaning`,`description`) VALUES (62,'FWIW','For what it\'s worth.','An opinion');
+INSERT INTO `` (`id`,`acronym`,`meaning`,`description`) VALUES (63,'SPGW','Sportsbook Gateway.','A Fanduel integration layer');
+INSERT INTO `` (`id`,`acronym`,`meaning`,`description`) VALUES (64,'PAS','Player Activity Stetement.','what is says on the tin');
+INSERT INTO `` (`id`,`acronym`,`meaning`,`description`) VALUES (65,'BYOB','Bring Your Own Beer','Standing rule for parties at my house…');
+INSERT INTO `` (`id`,`acronym`,`meaning`,`description`) VALUES (66,'FD','Fanduel','The Client.');
+INSERT INTO `` (`id`,`acronym`,`meaning`,`description`) VALUES (67,'FTW','For The Win','Use this to indicate that something is the best');
+INSERT INTO `` (`id`,`acronym`,`meaning`,`description`) VALUES (68,'HN','Hacker News','A.K.A. The Orange Site');
+INSERT INTO `` (`id`,`acronym`,`meaning`,`description`) VALUES (69,'LMGTFY','Let Me Google That For You','It might be worth googling before asking the question (in a nice way :-) )');
+INSERT INTO `` (`id`,`acronym`,`meaning`,`description`) VALUES (70,'NSFW','Not Suitable For Work','Unsuitable for consumption in a work environment. May contain swearing / politically incorrect language / adult content.');
+INSERT INTO `` (`id`,`acronym`,`meaning`,`description`) VALUES (71,'RTFM','Read The Friendly Manual','You/me/we/they can/could/should have found the requested information in the documentation');
+INSERT INTO `` (`id`,`acronym`,`meaning`,`description`) VALUES (72,'SFW','Suitable For Work','Suitable for consumption in a work environment, devoid of swearing, politically-incorrect language etc.');
+INSERT INTO `` (`id`,`acronym`,`meaning`,`description`) VALUES (73,'TLDR','Too Long, Didn’t Read','Its all a bit long - this would be a summary');
+INSERT INTO `` (`id`,`acronym`,`meaning`,`description`) VALUES (74,'XD','XDesign','Where we work!');
+INSERT INTO `` (`id`,`acronym`,`meaning`,`description`) VALUES (75,'YMMV','Your Mileage May Vary','You may see different results. As in It works on my machine');
+INSERT INTO `` (`id`,`acronym`,`meaning`,`description`) VALUES (76,'BDD','Behaviour Driven Development','Like TDD, but using the words “Given”, “When” and “Then” a lot.');
+INSERT INTO `` (`id`,`acronym`,`meaning`,`description`) VALUES (77,'DDD','Domain Driven Design','A software design methodology that puts the business domain at the forefront of the design process');
+INSERT INTO `` (`id`,`acronym`,`meaning`,`description`) VALUES (78,'DRY','Don’t Repeat Yourself','By which I mean, a thing (usually some code) is repetitive and IMO should be refactored.');
+INSERT INTO `` (`id`,`acronym`,`meaning`,`description`) VALUES (79,'NIH','Not Invented Here','Remember that project you worked on where they decided not to use Spring, and instead ended up writing a half-baked, barely-functional reimplementation of most of Spring? Well, the decision not to use Spring was likely because it was not invented here.');
+INSERT INTO `` (`id`,`acronym`,`meaning`,`description`) VALUES (81,'IMHO','In My Humble Opinion','Same as IMO, but not quite as passive aggresive');
+INSERT INTO `` (`id`,`acronym`,`meaning`,`description`) VALUES (82,'NPM','Node Package Manager','Javascript’s (well, node’s really) hellish package manager. If you encounter a project that’s still using this, run.');
+INSERT INTO `` (`id`,`acronym`,`meaning`,`description`) VALUES (83,'COK','Cat On Keyboard','A situation where something going wring can only be explained by a cat walking across your keyboard.');
+INSERT INTO `` (`id`,`acronym`,`meaning`,`description`) VALUES (84,'PICNIC','Problem In Chair, Not In Computer','Indicates that I/we/you/they caused the problem, and the software is fine. ');
+INSERT INTO `` (`id`,`acronym`,`meaning`,`description`) VALUES (85,'SOLID','SOLID Programming Principles','If I say this, I’m referring to the SOLID Principles for Object-Oriented design');
+INSERT INTO `` (`id`,`acronym`,`meaning`,`description`) VALUES (86,'SQ','SonarQube','A great build-time code-quality tool that is almost universally hated.');
+INSERT INTO `` (`id`,`acronym`,`meaning`,`description`) VALUES (87,'TDD','Test Driven Development','Expectation: the practice of writing software starting from tiny incremental tests, and only writing enough software to make the tests pass before refactoring or writing another test. Red/Green/Refactor! Reality: The practice of writing your tests in a carefully-planned iterative style so you can pretend you wrote them first.');
+INSERT INTO `` (`id`,`acronym`,`meaning`,`description`) VALUES (88,'YAGNI','You Ain’t Gonna Need It','If I say this it usually means that I don’t think we should do something.');
+INSERT INTO `` (`id`,`acronym`,`meaning`,`description`) VALUES (89,'YANG','You Are Not Google','When I say this, I’m probably suggesting that the highly-performant massively-scalable fault-tolerant Kafka-and-reactive-streams-based micro-services architecture that is being suggested would be great, at Google scale.');
+INSERT INTO `` (`id`,`acronym`,`meaning`,`description`) VALUES (92,'IMO','In Mikes Opinion',' Alternative view from Mike');
+INSERT INTO `` (`id`,`acronym`,`meaning`,`description`) VALUES (93,'WTF','What The Flip','Utter astonishment');
+INSERT INTO `` (`id`,`acronym`,`meaning`,`description`) VALUES (94,'MAB','Mike\'s Acronym Bot',' Hackronym - Expanding Acronyms since 2023!');
+INSERT INTO `` (`id`,`acronym`,`meaning`,`description`) VALUES (95,'LBT','Little Bobby Tables','Pseudonym of Robert \";DROP TABLE ACRONYMS;--');
+INSERT INTO `` (`id`,`acronym`,`meaning`,`description`) VALUES (96,'HPA','Horizontal Pod Autoscaling','A Kubernetes resource that increases or decreases the number of Pods depending on the current demand');
+INSERT INTO `` (`id`,`acronym`,`meaning`,`description`) VALUES (98,'MLS','Mutli Line Strings','I wonder\n\nhow it handles\n\nthis...');
+INSERT INTO `` (`id`,`acronym`,`meaning`,`description`) VALUES (100,'OOO','Out Of Office','I\'m away and likely unreachable');
+INSERT INTO `` (`id`,`acronym`,`meaning`,`description`) VALUES (101,'LGTM','Looks Great To Me',' Something that Tony the tiger might say after reviewing a PR');
+INSERT INTO `` (`id`,`acronym`,`meaning`,`description`) VALUES (102,'PR','Pull Request','Or maybe Peer Review??');
+INSERT INTO `` (`id`,`acronym`,`meaning`,`description`) VALUES (103,'JWST','James Webb Space Telescope','The largest space telescope out there today and successor to Hubble - helping us shed light on the murky early history of the Universe. Not to be confused with the Jake Webb Space Telescope.');
+INSERT INTO `` (`id`,`acronym`,`meaning`,`description`) VALUES (104,'ML','Machine Learning','A phrase commonly used to signify that a piece of software is better and shinier than its alternatives');
+INSERT INTO `` (`id`,`acronym`,`meaning`,`description`) VALUES (105,'GDP','Group Data Platform','FanDuel\'s data warehousing solution. Production services commonly push data into GDP using Automadata definitions. See also: DWH');
+INSERT INTO `` (`id`,`acronym`,`meaning`,`description`) VALUES (106,'DWH','Data Warehouse','An alternative name for GDP - the Group Data Platform which is FanDuel\'s data warehouse. May also refer to the previous data warehousing system. See also: GDP');
+INSERT INTO `` (`id`,`acronym`,`meaning`,`description`) VALUES (107,'TF','Terraform','Infrastructure-as-code language developed by Hashicorp. Used to deploy and manage infrastructure across many platforms such as AWS; Azure; GCP; Datadog');
+INSERT INTO `` (`id`,`acronym`,`meaning`,`description`) VALUES (108,'S3','Simple Storage Service','An AWS service that allows one to store and retrieve objects (arbitrary data files) in buckets (folders) in the cloud.');
+INSERT INTO `` (`id`,`acronym`,`meaning`,`description`) VALUES (109,'EC2','Elastic Compute Cloud','An AWS service that provides on-demand virtual machine instances along with features such as autoscaling and reusable launch templates');
+INSERT INTO `` (`id`,`acronym`,`meaning`,`description`) VALUES (110,'SNS','Simple Notification Service','An AWS service that offers a publisher-subscriber model; often used alongside other services such as SQS and Lambda');
+INSERT INTO `` (`id`,`acronym`,`meaning`,`description`) VALUES (111,'SQS','Simple Queue Service','An AWS service that offers a message queue model useful for asynchronous communication between distributed services. Supports features like FIFO queues and DLQs');
+INSERT INTO `` (`id`,`acronym`,`meaning`,`description`) VALUES (112,'AWS','Amazon Web Services','An ingenious system in which one pays Jeff Bezos for the pleasure of using his computers rather than use one\'s own. Comes with many associated benefits like reliability; scalability; interoperability; and doing away with the need to maintain physical infrastructure.');
+INSERT INTO `` (`id`,`acronym`,`meaning`,`description`) VALUES (113,'FIFO','First In First Out','A model in queue-based systems such as AWS and RabbitMQ which means that messages will be read in the same order as they were sent');
+INSERT INTO `` (`id`,`acronym`,`meaning`,`description`) VALUES (114,'DLQ','Dead-Letter Queue','A feature offered by queue-based systems such as AWS SQS and RabbitMQ in which messages that fail to be processed in a queue can be retained in a second queue for later analysis');
+INSERT INTO `` (`id`,`acronym`,`meaning`,`description`) VALUES (115,'TLA','Three-Letter Acronym','A term that describes itself in addition to many other entries in this bot');
+INSERT INTO `` (`id`,`acronym`,`meaning`,`description`) VALUES (116,'SPAG','Spelling Punctuation And Grammar','Especially important to master if one is writing acronyms and their definitions');
+INSERT INTO `` (`id`,`acronym`,`meaning`,`description`) VALUES (117,'JSON','JavaScript Object Notation','A data interchange format intended to be easily human-readable as well as easy for machines to parse - commonly used in web APIs');
+INSERT INTO `` (`id`,`acronym`,`meaning`,`description`) VALUES (118,'VFD','Volunteer Fire Department','A secret organisation renowned for its use of acronyms and dedicated to putting out fires - in both the literal and the metaphorical sense');
+INSERT INTO `` (`id`,`acronym`,`meaning`,`description`) VALUES (119,'GPT','Generative Pre-trained Transformer','The system whose eventual descendants are going to put us all out of a job');
+INSERT INTO `` (`id`,`acronym`,`meaning`,`description`) VALUES (121,'MITM','Man In The Middle','A man-in-the-middle attack is a cyberattack where the attacker secretly relays and possibly alters the communications between two parties who believe that they are directly communicating with each other');
+INSERT INTO `` (`id`,`acronym`,`meaning`,`description`) VALUES (123,'TTFN',' Ta Ta For Now',' A way of saying goodbye which came into prominence during the second world war.');
+INSERT INTO `` (`id`,`acronym`,`meaning`,`description`) VALUES (124,'*AFK*','Away From Keyboard','Away from computer for x amount of time and it\'s likely the person is not reachable');
+INSERT INTO `` (`id`,`acronym`,`meaning`,`description`) VALUES (125,'RAII','Resource Acquisition Is Initialization','A pattern used in C++ that helps avoid some of the more egregious foot-guns in that language');
+INSERT INTO `` (`id`,`acronym`,`meaning`,`description`) VALUES (127,'TCS','Totally Confusing Solutions','Where you pay alot of money, for a lot of gumph');
+INSERT INTO `` (`id`,`acronym`,`meaning`,`description`) VALUES (128,'VPN',' Virtual Private Network',' Required to connect to various FanDuel infrastructure/services. Why use one when you can try all of them?');
+INSERT INTO `` (`id`,`acronym`,`meaning`,`description`) VALUES (129,'CPP','Cross Product Promotions','A nightmarish hellscape of a FanDuel(ish) project you\'ll get sent to work on if you can\'t behave yourself');
+INSERT INTO `` (`id`,`acronym`,`meaning`,`description`) VALUES (130,'BING','Bing Is Not Google','Bing is a search engine provided by Microsoft currently being intergrated with AI and hilarious results!');
+INSERT INTO `` (`id`,`acronym`,`meaning`,`description`) VALUES (131,'SMOP',' Small Matter of Programming',' A slightly off-hand way of describing the work involved in implementing a new feature.');
